0b3726a8b8dae7a5f2eb5828c5a67c2865e9e6bd
h4rdfox <jatpac501@gmail.com>
2023-12-18 16:44:24 +0400

    ////

---
+++ database/migrations/2023_12_18_120408_create_timetable_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('timetable', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->integer('tutorID');
+            $table->integer('subjectID');
+            $table->integer('userID');
+            $table->boolean('isAccept')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('timetable');
+    }
+};
